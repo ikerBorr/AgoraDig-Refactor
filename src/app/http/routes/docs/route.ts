4079a15a42b45800fc5acc2d79ec6a2b3d307cff
ikerBorr <ikerborr@outlook.es>
2025-10-10 21:11:17 +0200

    change from swagger to stoplight

--- src/app/http/routes/docs/route.ts
+++ src/app/http/routes/docs/route.ts
@@ -1,40 +1,38 @@
-import type express from 'express'
-import YAML from 'yamljs'
-import path from 'path'
+import type express from 'express';
+import YAML from 'yamljs';
+import path from 'path';
 
 export async function GET(_req: express.Request, res: express.Response) {
-    const specPath = path.resolve(process.cwd(), './docs/api/openapi.yaml')
-    const spec = YAML.load(specPath)
-    const specJson = JSON.stringify(spec)
+    const specPath = path.resolve(process.cwd(), './docs/api/openapi.yaml');
+    const spec = YAML.load(specPath);
+    const specJson = JSON.stringify(spec);
 
     const html = `
-        <!DOCTYPE html>
+        <!doctype html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8" />
-            <title>API Docs</title>
-            <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <style>body { margin: 0; background: #fafafa; }</style>
-        </head>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+              <title>API Documentation</title>
+            
+              <script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+              <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+              <style>
+                    html, body, elements-api { height: 100%; margin: 0; }
+                    elements-api > div { width: 100%; }
+              </style>
+            </head>
         <body>
-            <div id="swagger-ui"></div>
-        
-            <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-            <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
-            <script>
-                window.onload = function () {
-                    SwaggerUIBundle({
-                        spec: ${specJson},
-                        dom_id: '#swagger-ui',
-                        deepLinking: true,
-                        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-                        layout: "StandaloneLayout"
-                    });
-                };
-            </script>
+              <elements-api id="docs" router="hash" style="display:flex; height:100vh; width:100vw"/>
+            
+              <script type="application/json" id="openapi-spec">${specJson}</script>
+              <script>
+                    const spec = JSON.parse(document.getElementById('openapi-spec').textContent);
+                    document.getElementById('docs').apiDescriptionDocument = spec;
+              </script>
         </body>
-        </html>`.trim()
+        </html>
+    `;
 
-    res.status(200).type('html').send(html)
+    res.status(200).type('html').send(html);
 }
