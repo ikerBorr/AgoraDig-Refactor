2369c38ba89977f90dfed81c7e926d08a42eb4ac
ikerBorr <ikerborr@outlook.es>
2025-11-02 22:15:14 +0100

    format

--- src/contexts/shared-kernel/domain/ports/cache.ts
+++ src/contexts/shared-kernel/domain/ports/cache.ts
@@ -1,7 +1,7 @@
-import type {Nullable} from "@/contexts/shared-kernel/application/types/nullable";
+import type { Nullable } from '@/contexts/shared-kernel/application/types/nullable'
 
 export interface Cache {
-    get<T>(key: string): Promise<Nullable<T>>;
-    set<T>(key: string, value: T, ttlSeconds?: number): Promise<void>;
-    del(key: string): Promise<void>;
+    get<T>(key: string): Promise<Nullable<T>>
+    set<T>(key: string, value: T, ttlSeconds?: number): Promise<void>
+    del(key: string): Promise<void>
 }
--- src/contexts/shared-kernel/infrastructure/cache/redis.cache.ts
+++ src/contexts/shared-kernel/infrastructure/cache/redis.cache.ts
@@ -1,23 +1,23 @@
-import type {Cache} from "@/contexts/shared-kernel/domain/ports/cache";
-import {RedisClient} from "@/contexts/shared-kernel/infrastructure/cache/redis-client";
+import type { Cache } from '@/contexts/shared-kernel/domain/ports/cache'
+import { RedisClient } from '@/contexts/shared-kernel/infrastructure/cache/redis-client'
 
 export abstract class RedisCache implements Cache {
     async get<T>(key: string): Promise<T | null> {
-        const data = await (await RedisClient.getInstance()).get(key);
-        return data ? (JSON.parse(data) as T) : null;
+        const data = await (await RedisClient.getInstance()).get(key)
+        return data ? (JSON.parse(data) as T) : null
     }
 
     async set<T>(key: string, value: T, ttlSeconds?: number): Promise<void> {
-        const serialized = JSON.stringify(value);
+        const serialized = JSON.stringify(value)
 
         if (ttlSeconds && ttlSeconds > 0) {
-            await (await RedisClient.getInstance()).setEx(key, ttlSeconds, serialized);
+            await (await RedisClient.getInstance()).setEx(key, ttlSeconds, serialized)
         } else {
-            await (await RedisClient.getInstance()).set(key, serialized);
+            await (await RedisClient.getInstance()).set(key, serialized)
         }
     }
 
     async del(key: string): Promise<void> {
-        await (await RedisClient.getInstance()).del(key);
+        await (await RedisClient.getInstance()).del(key)
     }
 }
