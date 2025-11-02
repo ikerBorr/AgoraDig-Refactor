4c28287ed531fb5bc25d6f5723e45779c340f66e
ikerBorr <ikerborr@outlook.es>
2025-11-02 21:43:25 +0100

    format

--- src/app/api/routes/welcome/route.ts
+++ src/app/api/routes/welcome/route.ts
@@ -2,5 +2,5 @@ import type express from 'express'
 import path from 'node:path'
 
 export async function GET(_req: express.Request, res: express.Response) {
-    res.sendFile(path.join(__dirname, '/../../../../../public', 'index.html'));
+    res.sendFile(path.join(__dirname, '/../../../../../public', 'index.html'))
 }
